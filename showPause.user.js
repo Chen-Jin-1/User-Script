951bc887a2a3e824262c8d67bd823bdca0bc1352
Chen-Jin <cj3663@outlook.com>
2025-10-06 16:14:34 +0800

    Create showPause.user.js

---
+++ showPause.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         显示暂停
+// @namespace    cj-show-pause
+// @match        https://www.ccw.site/detail/*
+// @grant        none
+// @version      0
+// @author       Chen-Jin
+// @description  防止作品隐藏暂停按钮
+// @icon         https://m.ccw.site/user_projects_assets/6df19377-8e84-4929-9e9c-93ddca968dcc.png
+// @updateURL    data:
+// ==/UserScript==
+
+var style = document.createElement('style');
+style.innerHTML = '.action-item-P9SP6.action-control-8nmb9 {visibility: visible !important;}';
+document.head.appendChild(style);
