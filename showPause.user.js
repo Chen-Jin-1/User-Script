1946c0b4fff07f4f82284643a93982a2ca1788d1
Chen-Jin <cj3663@outlook.com>
2025-10-06 16:16:24 +0800

    Update showPause.user.js

--- showPause.user.js
+++ showPause.user.js
@@ -1,13 +1,14 @@
 // ==UserScript==
-// @name         显示暂停
-// @namespace    cj-show-pause
-// @match        https://www.ccw.site/detail/*
-// @grant        none
-// @version      0
-// @author       Chen-Jin
-// @description  防止作品隐藏暂停按钮
-// @icon         https://m.ccw.site/user_projects_assets/6df19377-8e84-4929-9e9c-93ddca968dcc.png
-// @updateURL    data:
+// @name                显示暂停
+// @namespace           cj-show-pause
+// @match               https://www.ccw.site/detail/*
+// @grant               none
+// @version             1.0.0
+// @author              Chen-Jin
+// @description         防止作品隐藏暂停按钮
+// @icon                https://m.ccw.site/user_projects_assets/6df19377-8e84-4929-9e9c-93ddca968dcc.png
+// @updateURL           https://bgithub.xyz/Chen-Jin-1/User-Scripts/raw/refs/heads/main/showPause.user.js
+// @downloadURL         https://bgithub.xyz/Chen-Jin-1/User-Scripts/raw/refs/heads/main/showPause.user.js
 // ==/UserScript==
 
 var style = document.createElement('style');
