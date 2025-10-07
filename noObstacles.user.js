d0c5056b1888e674b53dce00e7a05f92da1503a8
Chen-Jin <cj3663@outlook.com>
2025-10-07 11:41:23 +0800

    Update and rename noCookies.user.js to noObstacles.user.js

--- noObstacles.user.js
+++ noObstacles.user.js
@@ -1,14 +1,14 @@
 // ==UserScript==
-// @name                No Cookies
-// @namespace           cj-no-cookies
+// @name                No Obstacles
+// @namespace           cj-no-obstacles
 // @match               *://*/*
 // @grant               none
 // @version             0.0.1
 // @author              Chen-Jin
-// @description         不显示 Cookies 窗口
+// @description         隐藏网页障碍
 // @icon                https://m.ccw.site/user_projects_assets/6df19377-8e84-4929-9e9c-93ddca968dcc.png
-// @updateURL           https://bgithub.xyz/Chen-Jin-1/User-Scripts/raw/refs/heads/main/noCookies.user.js
-// @downloadURL         https://bgithub.xyz/Chen-Jin-1/User-Scripts/raw/refs/heads/main/noCookies.user.js
+// @updateURL           https://bgithub.xyz/Chen-Jin-1/User-Scripts/raw/refs/heads/main/noObstacles.user.js
+// @downloadURL         https://bgithub.xyz/Chen-Jin-1/User-Scripts/raw/refs/heads/main/noObstacles.user.js
 // ==/UserScript==
 
 var style = document.createElement('style');
