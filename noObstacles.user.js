477a4bd6d5b9fc404b6d4e46f36774a0a6a9f28c
Chen-Jin <cj3663@outlook.com>
2025-10-07 11:52:07 +0800

    Update noObstacles.user.js

--- noObstacles.user.js
+++ noObstacles.user.js
@@ -3,7 +3,7 @@
 // @namespace           cj-no-obstacles
 // @match               *://*/*
 // @grant               none
-// @version             0.0.3
+// @version             0.0.4
 // @author              Chen-Jin
 // @description         隐藏网页障碍
 // @icon                https://m.ccw.site/user_projects_assets/6df19377-8e84-4929-9e9c-93ddca968dcc.png
@@ -12,5 +12,5 @@
 // ==/UserScript==
 
 var style = document.createElement('style');
-style.innerHTML = '.LBktY > .GssDD, #cookieConsentContainer, .mw-cookiewarning-container, #wcpConsentBannerCtrl, banner-3yTl4 {display: none !important;}';
+style.innerHTML = '.LBktY > .GssDD, #cookieConsentContainer, .mw-cookiewarning-container, #wcpConsentBannerCtrl, .banner-3yTl4 {display: none !important;}';
 document.head.appendChild(style);
